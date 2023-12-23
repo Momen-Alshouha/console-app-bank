829ba62ff9fee428218ca277f1fa4c4fa579d5e1
momen-alshouha <momenalshouha@outlook.com>
2023-12-23 15:44:44 +0300

    Add project files.

---
+++ console-app-bank.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "clsDate.h"
+#include "clsInputValidation.h"
+#include "clsPeriod.h"
+#include "clsString.h"
+#include "clsUtil.h"
+using namespace std;
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+
+}
